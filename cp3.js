1c30382f43851d5aeee922b3c3ba5dd076413c1f
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-05-22 01:35:44 -0300

    Criando a primeira função para o botão idFirstName

---
+++ cp3.js
@@ -0,0 +1,15 @@
+const inputFirstName = document.querySelector("#idFirstName");
+
+inputFirstName.addEventListener("keyup", () => {
+
+    const lblFirstName = document.querySelector("label[for='idFirstName']");
+
+    if (inputFirstName.value.length) {
+        inputFirstName.setAttribute("style", "outline-color:#ff0000;");
+        
+    } else {
+        inputFirstName.setAttribute("style", "outline-color:#00ff00;");
+        
+    }
+
+});
