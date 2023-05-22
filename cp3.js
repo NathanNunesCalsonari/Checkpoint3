9ea6ae13193f6699158f68a9912da240e93ca87d
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-05-22 01:39:54 -0300

    Acrescentando o botão idEmail

--- cp3.js
+++ cp3.js
@@ -29,3 +29,21 @@ inputLastName.addEventListener("keyup", () => {
     }
 
 });
+
+const inputEmail = document.querySelector("#idEmail");
+
+
+inputEmail.addEventListener("keyup", () => {
+
+    const lblEmail = document.querySelector("label[for='idEmail']");
+
+    if (inputEmail.value.length) {
+        inputEmail.setAttribute("style", "outline-color:#ff0000;");
+        lblEmail.setAttribute("style", "color:#ff0000;");
+    } else {
+        inputEmail.setAttribute("style", "outline-color:#00ff00;");
+        lblEmail.setAttribute("style", "color:#00ff00;");
+
+    }
+
+});
