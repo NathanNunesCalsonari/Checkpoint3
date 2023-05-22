890349326d808926ed8d8c967f2031df0e05d5b5
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-05-22 01:37:42 -0300

    Acrescentando o lblFirstName com o setAttribute

--- cp3.js
+++ cp3.js
@@ -6,10 +6,10 @@ inputFirstName.addEventListener("keyup", () => {
 
     if (inputFirstName.value.length) {
         inputFirstName.setAttribute("style", "outline-color:#ff0000;");
-        
+        lblFirstName.setAttribute("style", "color:#ff0000;");
     } else {
         inputFirstName.setAttribute("style", "outline-color:#00ff00;");
-        
+        lblFirstName.setAttribute("style", "color:#00ff00;");
     }
 
 });
